4505ce674a7f570240356546282bb5279c1f8eb4
Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
2018-06-05 23:42:09 +0200

    Rasa Integration for errbot plugin

--- plugin/rasa.py
+++ plugin/rasa.py
@@ -1,6 +1,11 @@
 from errbot import BotPlugin
+import rasa_core
+from rasa_core.agent import Agent
+agent = Agent.load('./models/dialogue')
 
 class Rasa(BotPlugin):
     def callback_message(self, message):
         self.log.debug(message.body)
-        self.send(getattr(message.frm, 'room', message.frm), message.body)
+        response = agent.handle_message(message.body)
+        self.log.debug(response)
+        self.send(getattr(message.frm, 'room', message.frm), response[0]["text"])
