04770d41bd6563e12b05f0a2ceb60ec4e7f54fbf
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-06-06 01:07:03 +0200

    add dict creation to model setup, use agend

--- create_models.sh
+++ create_models.sh
@@ -1,3 +1,8 @@
+echo "Download space dictionary"
+python -m spacy download en_core_web_md
+echo "link it"
+python -m spacy link en_core_web_md en
+
 python nlu_model.py
 
 
--- plugin/rasa.py
+++ plugin/rasa.py
@@ -9,20 +9,14 @@ from rasa_core.policies.memoization import MemoizationPolicy
 from rasa_core.featurizers import (MaxHistoryTrackerFeaturizer,
                                    BinarySingleStateFeaturizer)
 class Rasa(BotPlugin):
-    def activate(self):
-        logging.basicConfig(level='INFO')
-        dialog_training_data_file = '/home/maximilian/Barcamp/2018/errbot/slack-plugin/config/stories.md'
-        path_to_model = '/home/maximilian/Barcamp/2018/errbot/slack-plugin//models/dialogue'
-        # domain = Domain()
-        featurizer = MaxHistoryTrackerFeaturizer(BinarySingleStateFeaturizer(), max_history=5)
-        self.agent = Agent('/home/maximilian/Barcamp/2018/errbot/slack-plugin//config/chat_domain.yml',
-                    policies=[MemoizationPolicy(max_history=5),
-                                KerasPolicy(featurizer)])
-
     def callback_message(self, message):
         self.log.debug(message.body)
-        result = self.agent.handle_message(message.body)
-        self.log(result)
 
+        domain = __file__+'/../config/chat_domain.yml'
+        interpreterPath = __file__+'/../models/nlu/default/chat'
+        agent = Agent(domain, interpreter=interpreterPath)
+        self.log.debug('Agent created')
+        result = agent.handle_message(message.body)
+        self.log.debug('done')
+        self.log.debug(result)
         self.send(getattr(message.frm, 'room', message.frm), result)
-        self.send(getattr(message.frm, 'room', message.frm), message.body)
