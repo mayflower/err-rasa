732d7fcadac7585dee9835ea9db88c3eb0358921
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-06-06 12:50:10 +0200

    let it work

--- plugin/rasa.py
+++ plugin/rasa.py
@@ -1,28 +1,35 @@
+import random
 import logging
-import rasa_core
 
-from errbot import BotPlugin
-from rasa_nlu.model import Metadata, Interpreter
-from rasa_core.agent import Agent
+from rasa_nlu.model import Trainer, Metadata, Interpreter
 from rasa_nlu.components import ComponentBuilder
-from rasa_core.policies.keras_policy import KerasPolicy
-from rasa_core.policies.memoization import MemoizationPolicy
-from rasa_core.featurizers import (MaxHistoryTrackerFeaturizer,
-                                   BinarySingleStateFeaturizer)
+from errbot import BotPlugin
+
 class Rasa(BotPlugin):
     def callback_message(self, message):
-        self.log.debug(message.body)
-        self.send(getattr(message.frm, 'room', message.frm), message.body)
-        from rasa_nlu.model import Metadata, Interpreter
-        
+        self.log.debug(message.body)        
         builder = ComponentBuilder(use_cache=True)
-        model_directory = './models/nlu/default/chat';
+        model_directory = './models/nlu/default/chat'
         interpreter = Interpreter.load(model_directory, builder)
-        result = interpreter.parse(message.body)
-        self.log.debug(result)
-        self.send(getattr(message.frm, 'room', message.frm), 'RESULT:')
-        intent = getattr(result, 'intent')
-        self.send(getattr(message.frm, 'room', message.frm), 'Name: '+getattr(intent, 'name', 'No Name'))
-        self.send(getattr(message.frm, 'room', message.frm), 'Confidence: '+getattr(intent, 'confidence', 'No Confidence'))
+        res = interpreter.parse(message.body)
+        self.log.info('rasa parse res: {}'.format(res))
+        if res['intent']:
+            if res['intent']['name']:
+                name = res['intent']['name']
+                self.log.debug('Name: '+name)    
+                self.send(getattr(message.frm, 'room', message.frm), 'Name: '+name)
+            else:
+                self.log.debug('No name set')    
+                self.send(getattr(message.frm, 'room', message.frm), 'No name set')    
+            
+            if res['intent']['confidence']:
+                confidence = str(res['intent']['confidence'])
+                self.log.debug('Name: '+confidence)    
+                self.send(getattr(message.frm, 'room', message.frm), 'Confidence: '+confidence)
+            else:
+                self.log.debug('No name set')    
+                self.send(getattr(message.frm, 'room', message.frm), 'No confidence set')  
 
-        
+        else:
+            self.log.debug('No intent set')    
+            self.send(getattr(message.frm, 'room', message.frm), 'No intent set')
