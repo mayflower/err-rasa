e4ad5e72941112f31e8dfd4ba42e0e17e83d456b
ElectricMaxxx <maximilian.berghoff@gmx.de>
2018-06-07 09:22:48 +0200

    new trainings

--- actions.py
+++ actions.py
@@ -1,9 +1,11 @@
+import logging
 from rasa_core.actions import Action
 
 class ActionOrderPizza(Action):
     def name(self):
         return 'action_order_pizza'
     def run(self, dispatcher, tracker, domain):
+        logging.debug("Domain: {}".format(domain));
         dispatcher.utter_message('Lets order some pizza')
         return []
 
@@ -11,5 +13,6 @@ class ActionPedant(Action):
     def name(self):
         return 'action_pedant'
     def run(self, dispatcher, tracker, domain):
+        logging.debug("Domain: {}".format(domain));
         dispatcher.utter_message('You are not concrete enough')
         return []
--- config/stories.md
+++ config/stories.md
@@ -1,8 +1,12 @@
-## Story start 
+## Story greet 
 * greet
    - utter_greet
+
+## Story pedant
 * pedant
    - action_pedant
+
+# Story order_pizza
 * order_pizza
    - utter_get_pizza_size
 * order_pizza
